602cea03a15d12e4622e09473df59dd3b29a4796
kkondracikowski@imago3d.pl <kkondracikowski@imago3d.pl>
2021-11-21 21:08:49 +0100

    Add second module, KNN algorithm. Classification mode

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ from pandas.core import base
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from random import seed
+from random import randrange
+from math import dist, sqrt
 
+from streamlit.elements.arrow import Data
 
 def firstModule():
     st.subheader('Moduł 1')
@@ -226,7 +231,158 @@ def firstModule():
     )
 
     st.plotly_chart(fig)
+
+# Euclidean distance
+def euclideanDistance(data, newObj):
+    distance = []
+    for index, row in data.iloc[:,:-1].iterrows():
+        sum = 0.0
+        for i in range(len(newObj)):
+            sum += (row[i] - newObj[i])**2
+        
+        distance.append(sqrt(sum))
     
+    return distance
+
+# Manhattan distance
+def manhattanDistance(data, newObj):
+    distance = []
+
+    for index, row in data.iloc[:,:-1].iterrows():
+        sum = 0.0
+        for i in range(len(newObj)):
+            sum += abs(row[i] - newObj[i])
+            
+        distance.append(sum)
+        
+    return distance
+
+# Chebyshev distance
+def chebyshevDistance(data, newObj):
+    distance = []
+
+    for index, row in data.iloc[:,:-1].iterrows():
+        listOfDistances = []
+        for i in range(len(newObj)):
+            listOfDistances.append(row[i]-newObj[i])
+            maxDistance = max(listOfDistances)
+        distance.append(maxDistance)
+
+    return distance
+
+# Mahalanobis distance
+def mahalanobisDistance(data, newObj):
+    distance = []
+    v_m = []
+    cov_data = np.cov(data.iloc[:,:-1].T)
+    
+    for index, row in data.iloc[:,:-1].iterrows():
+        v_m = []
+        for i in range(len(newObj)):
+            v_m.append(newObj[i] - row[i])
+        
+        result = np.array(v_m)
+        result = np.dot(result, np.linalg.inv(cov_data))
+        result = np.dot(result, v_m)
+
+        distance.append(np.sqrt(result))
+    
+    return distance
+
+# get k neighbors
+def getNeighbors(data, metric, k):
+    neighbors = []
+    # specify column to sort and get ksmallest distances
+    if metric == 'Euklidesowa':
+        neighbors = data.sort_values(by='Metryka Euklidesowa').head(k)
+        
+    elif metric == 'Manhattan':
+        neighbors =  data.sort_values(by='Metryka Manhattan').head(k)
+        
+    elif metric == 'Czebyszewa':
+        neighbors =data.sort_values(by='Metryka Chebysheva').head(k)
+        
+    elif metric == 'Mahalanobisa':
+        neighbors = data.sort_values(by='Metryka Mahalanobisa').head(k)
+    
+    return neighbors
+
+def resolveConflicts(data,neighbors,k,classColumn,potentialConflicts, metric,newObject):
+    
+    potentialConflictsSums = []
+    minSumsOfPotentialConflicts = []
+    
+    for n in potentialConflicts:
+        sum = 0.0
+        sum = neighbors.loc[neighbors[classColumn] == n].iloc[:,-1:].sum()
+        potentialConflictsSums.append([n, sum[0]])
+
+    minVal = potentialConflictsSums[0][1]
+    
+    # Searching for the minimum sum 
+    for name, sumVal in potentialConflictsSums:
+        if sumVal < minVal:
+            minVal = sumVal
+    
+    # If sum == minimum we assign the class
+    for name, sumVal in potentialConflictsSums:
+        if sumVal == minVal:
+            minSumsOfPotentialConflicts.append([name, sumVal])
+    
+    # Checking if there is more than one minimum sum of distances
+    if (len(minSumsOfPotentialConflicts) > 1): # If so, KNN for k+1
+        KNN(data,newObject,metric,k,classColumn)
+    elif (len(minSumsOfPotentialConflicts) == 1 ): # If not, we return a prediction
+        predict = minSumsOfPotentialConflicts[0][0]
+        
+    return predict
+
+# Predykcja
+def predictClassification(data, neighbors, k, classColumn, metric, newObject):
+    df_value_counts = neighbors[classColumn].value_counts().reset_index()
+    df_value_counts.columns = ['name', 'counts']
+    maxVal = df_value_counts['counts'].max()
+    
+    potentialConflicts = []
+    potentialConflictsSums = []
+    classOfNewObj = ' '
+
+    for index, row in df_value_counts.iterrows():
+        if(row['counts'] == maxVal):
+            potentialConflicts.append(row['name'])
+
+    if (len(potentialConflicts) > 1):
+        classOfNewObj = resolveConflicts(data, neighbors, k, classColumn,potentialConflicts, metric, newObject)
+    elif (len(potentialConflicts) == 1):
+        classOfNewObj = potentialConflicts[0]
+    return classOfNewObj
+     
+# Alogrithm
+def KNN(data, newObject, metric, k, classColumn):
+    # 1st step - distance between objects - euclidean/manhattan/chebyshev/mahalanobisDistance
+    # 2nd step - sort data by distances                - getNeighbors()
+    # 3rd step - get K objects with smallest distances - getNeighbors()
+    # 4th step - resolve conflicts - predictClassification() -> resolveConflicts()
+    
+    if metric == 'Euklidesowa':
+        data['Metryka Euklidesowa']  = euclideanDistance(data, newObject)
+        
+    elif metric == 'Manhattan':
+        data['Metryka Manhattan'] = manhattanDistance(data, newObject)
+        
+    elif metric == 'Czebyszewa':
+        data['Metryka Chebysheva'] = chebyshevDistance(data, newObject)
+        
+    elif metric == 'Mahalanobisa':
+        data['Metryka Mahalanobisa'] = mahalanobisDistance(data, newObject)
+
+    neighbors = getNeighbors(data, metric, k)
+    
+    prediction = predictClassification(data, neighbors, k, classColumn, metric, newObject)
+    
+    newObject.append(prediction)
+    st.write(newObject)
+
 def secondModule():
     st.subheader('Moduł 2')
     uploaded_file = st.file_uploader("", type="xlsx")
@@ -237,18 +393,50 @@ def secondModule():
         data = pd.read_csv('data/income.csv', sep =';', decimal=',')
     
     st.dataframe(data)
+    st.markdown('---')
     
     modes = ['Klasyfikacja', 'Ocena jakości klasyfikacji']
+
     mode = st.selectbox(
         'Wybierz tryb modułu',
         modes
     )
     
+    classColumn = st.selectbox(
+        'Wybierz kolumnę decyzyjną',
+        data.columns.values
+    )
+    
     if mode == modes[0]: #Klasyfikacja
-        st.write(modes[0])
         
+       leftColumn, rightColumn =  st.columns(2)
+       newObject = []
+       
+       with leftColumn: 
+            for col in data.columns[:-1]:
+                n = st.number_input(
+                    col,
+                    step = 1.0,
+                    format="%.2f"
+                )
+                newObject.append(n)     
+       with rightColumn:
+            metric = st.selectbox(
+                'Wybierz metrykę',
+                ['Euklidesowa', 'Manhattan', 'Czebyszewa', 'Mahalanobisa']
+            )
+            
+            k = st.number_input(
+                'Liczba sąsiadów',
+                value=3,
+                step=1
+            ) 
+            
+            if st.button('Klasyfikuj obiekt'):
+                KNN(data, newObject, metric, k, classColumn)
+
     elif mode == modes[1]: #Ocena jakości klasyfikacji
-        st.write(modes[1])
+        
     
 
 def main():
